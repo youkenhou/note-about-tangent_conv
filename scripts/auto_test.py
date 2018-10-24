979b0c77f72ce2543432ab58d33608b2b1be4d48
youkenhou <32795842+youkenhou@users.noreply.github.com>
2018-10-24 20:01:11 +0800

    分割测试集，自动分段运行

---
+++ scripts/auto_test.py
@@ -0,0 +1,64 @@
+import os
+
+json_path = "/ali_data/experiments/ali"
+
+print("start 1")
+cmd1 = "python3 tc.py " + os.path.join(json_path, "config1.json") + " --test"
+os.system(cmd1)
+
+print("start 2")
+cmd2 = "python3 tc.py " + os.path.join(json_path, "config2.json") + " --test"
+os.system(cmd2)
+
+print("start 3")
+cmd3 = "python3 tc.py " + os.path.join(json_path, "config3.json") + " --test"
+os.system(cmd3)
+
+print("start 4")
+cmd4 = "python3 tc.py " + os.path.join(json_path, "config4.json") + " --test"
+os.system(cmd4)
+
+print("start 5")
+cmd5 = "python3 tc.py " + os.path.join(json_path, "config5.json") + " --test"
+os.system(cmd5)
+
+print("start 6")
+cmd6 = "python3 tc.py " + os.path.join(json_path, "config6.json") + " --test"
+os.system(cmd6)
+
+print("start 7")
+cmd7 = "python3 tc.py " + os.path.join(json_path, "config7.json") + " --test"
+os.system(cmd7)
+
+print("start 8")
+cmd8 = "python3 tc.py " + os.path.join(json_path, "config8.json") + " --test"
+os.system(cmd8)
+
+print("start 9")
+cmd9 = "python3 tc.py " + os.path.join(json_path, "config9.json") + " --test"
+os.system(cmd9)
+
+print("start 10")
+cmd10 = "python3 tc.py " + os.path.join(json_path, "config10.json") + " --test"
+os.system(cmd10)
+
+print("start 11")
+cmd11 = "python3 tc.py " + os.path.join(json_path, "config11.json") + " --test"
+os.system(cmd11)
+
+print("start 12")
+cmd12 = "python3 tc.py " + os.path.join(json_path, "config12.json") + " --test"
+os.system(cmd12)
+
+print("start 13")
+cmd13 = "python3 tc.py " + os.path.join(json_path, "config13.json") + " --test"
+os.system(cmd13)
+
+print("start 14")
+cmd14 = "python3 tc.py " + os.path.join(json_path, "config14.json") + " --test"
+os.system(cmd14)
+
+print("start 15")
+cmd15 = "python3 tc.py " + os.path.join(json_path, "config15.json") + " --test"
+os.system(cmd15)
+
